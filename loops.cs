843bfb3dab7684baa538bfae1d257a9e21dc07e7
Farha-Nousheen <84795247+Farha-Nousheen@users.noreply.github.com>
2021-08-26 11:49:44 +0530

    Add files via upload

---
+++ loops.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int day = 1;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("MONDAY");
+                    break;
+                case 2:
+                    Console.WriteLine("tues");
+                    break;
+                case 3:
+                    Console.WriteLine("wed");
+                    break;
+                case 4:
+                    Console.WriteLine("thurs");
+                    break;
+                case 5:
+                    Console.WriteLine("friday");
+                    break;
+
+            }
+        }
+    }
+}
