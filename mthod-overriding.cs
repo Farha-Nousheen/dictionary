6d161ba48edf8254eb9e5d276ed34077c49f43aa
Farha-Nousheen <84795247+Farha-Nousheen@users.noreply.github.com>
2021-08-27 09:21:48 +0530

    Add files via upload

---
+++ mthod-overriding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class food
+{
+    public virtual void eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+public class pizza : food
+{
+    public override void eat()
+    {
+        Console.WriteLine("Eating pizza...");
+    }
+}
+public class TestOverriding
+{
+    public static void Main()
+    {
+        pizza p = new pizza();
+        p.eat();
+    }
+}
